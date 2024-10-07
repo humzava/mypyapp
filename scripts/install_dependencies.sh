ee1d80b84b799483783ebd62a38488c86bac21b9
humzava <humzafidava@gmail.com>
2024-10-07 06:20:16 +0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo systemctl stop python_app.service
 cd /home/ubuntu/python_app  # Change this to your application's directory
 sudo apt install python3-pip -y
 sudo apt install python3-venv
@@ -7,4 +6,3 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r /home/ubuntu/python_app/requirements.txt
 exit
-sudo systemctl start python_app.service
