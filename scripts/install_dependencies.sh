7bb2f2701606e1fb9287651288d5a250f6382c04
humzava <humzafidava@gmail.com>
2024-10-07 06:23:39 +0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,5 +6,5 @@ sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r /home/ubuntu/python_app/requirements.txt
-sudo systemctl start python_app.service
 exit
+sudo systemctl start python_app.service
