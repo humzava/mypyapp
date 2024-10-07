f560ee605740635b19b2c12ea1e496fbf114b009
humzava <humzafidava@gmail.com>
2024-10-07 06:33:15 +0500

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-pkill gunicorn  # Stop all Gunicorn processes
+
+LOGFILE=/home/ubuntu/python_app/scripts/stop_server.log
+
+{
+    echo "Stopping Gunicorn server..."
+    if pgrep -f gunicorn > /dev/null; then
+        pkill gunicorn
+        echo "Gunicorn server stopped."
+    else
+        echo "No Gunicorn server is running."
+    fi
+} &>> "$LOGFILE"
