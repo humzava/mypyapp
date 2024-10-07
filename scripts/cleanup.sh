b6b4f85c8cbe93ae291953f23e983556438f410d
humzava <humzafidava@gmail.com>
2024-10-07 06:15:10 +0500

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+# Clean up old application files
+echo "Cleaning up old files in /home/ubuntu/python_app/"
+rm -rf /home/ubuntu/python_app/*  # Remove all files in the application directory
